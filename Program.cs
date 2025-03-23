6661baad1a917f59d9b74a00b15fe35d415dd0f3
ttetricas <estermeireles89@gmail.com>
2025-03-22 22:34:27 -0300

    FOI

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int num1, num2, subtracao;
+
+Console.Write("num1: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("num2: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+subtracao = num1 - num2;
+
+Console.WriteLine($" {num1} - {num2} = {subtracao}");
